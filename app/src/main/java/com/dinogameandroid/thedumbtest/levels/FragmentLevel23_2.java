49cdfbe5aeacd2ee6e23f92eaec5d186409115b4
androidzakzak <androidzakzak@gmail.com>
2016-06-29 19:13:27 +0500

    bug fixed

--- app/src/main/java/com/dinogameandroid/thedumbtest/levels/FragmentLevel23_2.java
+++ app/src/main/java/com/dinogameandroid/thedumbtest/levels/FragmentLevel23_2.java
@@ -16,28 +16,36 @@ import com.dinogameandroid.thedumbtest.activity.GameActivity;
 public class FragmentLevel23_2 extends Fragment implements View.OnClickListener{
     private TextView tvLevel;
     private TextView tvStrikes;
-    private ImageView buttonRed;
-    private ImageView buttonBlue;
-    private ImageView buttonGreen;
-    private ImageView buttonYellow;
-    private TextView button24;
+    private ImageView redTriagle;
+    private ImageView redCircle;
+    private ImageView yellowSquare;
+    private ImageView yellowCircle;
+    private ImageView blueTriagle;
+    private ImageView redSquare;
+    private ImageView blueCircle;
     int count;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View mRootView = inflater.inflate(R.layout.fragment_level_23_2, container, false);
-        buttonRed = (ImageView) mRootView.findViewById(R.id.buttonRed);
-        buttonBlue = (ImageView) mRootView.findViewById(R.id.buttonBlue);
-        buttonGreen = (ImageView) mRootView.findViewById(R.id.buttonGreen);
-        buttonYellow = (ImageView) mRootView.findViewById(R.id.buttonYellow);
-        button24 = (TextView) mRootView.findViewById(R.id.button24);
-        button24.setOnClickListener(this);
-        buttonRed.setOnClickListener(this);
-        buttonBlue.setOnClickListener(this);
-        buttonGreen.setOnClickListener(this);
-        buttonYellow.setOnClickListener(this);
+        redTriagle = (ImageView) mRootView.findViewById(R.id.redTriagle);
+        redCircle = (ImageView) mRootView.findViewById(R.id.redCircle);
+        yellowSquare = (ImageView) mRootView.findViewById(R.id.yellowSquare);
+        yellowCircle = (ImageView) mRootView.findViewById(R.id.yellowCircle);
+        blueTriagle = (ImageView) mRootView.findViewById(R.id.blueTriagle);
+        redSquare = (ImageView) mRootView.findViewById(R.id.redSquare);
+        blueCircle = (ImageView) mRootView.findViewById(R.id.blueCircle);
+
+        redTriagle.setOnClickListener(this);
+        redCircle.setOnClickListener(this);
+        yellowSquare.setOnClickListener(this);
+        yellowCircle.setOnClickListener(this);
+        blueTriagle.setOnClickListener(this);
+        redSquare.setOnClickListener(this);
+        blueCircle.setOnClickListener(this);
         tvLevel=(TextView)mRootView.findViewById(R.id.tvLevel);
+        tvLevel.setOnClickListener(this);
         tvStrikes=(TextView)mRootView.findViewById(R.id.tvStrikes);
         tvLevel.setText(""+((GameActivity)getContext()).getLevel());
         tvStrikes.setText("Strikes:" + ((GameActivity) getContext()).getStrikes());
@@ -47,8 +55,69 @@ public class FragmentLevel23_2 extends Fragment implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.buttonRed:
-
+            case R.id.redTriagle:
+if(count==0){
+    count++;
+}
+                else {
+    ((GameActivity) getActivity()).setStrikes( (((GameActivity) getActivity()).getStrikes()+1));
+    tvStrikes.setText("Strikes:" + ((GameActivity) getActivity()).getStrikes());
+                }
+                break;
+            case R.id.redCircle:
+                if(count==5 ){
+                    count++;
+                }
+                else {
+                    ((GameActivity) getActivity()).setStrikes( (((GameActivity) getActivity()).getStrikes()+1));
+                    tvStrikes.setText("Strikes:" + ((GameActivity) getActivity()).getStrikes());
+                }
+                break;
+            case R.id.yellowSquare:
+                ((GameActivity) getActivity()).setStrikes( (((GameActivity) getActivity()).getStrikes()+1));
+                tvStrikes.setText("Strikes:" + ((GameActivity) getActivity()).getStrikes());
+                break;
+            case R.id.yellowCircle:
+                if(count==6){
+                    count++;
+                    tvLevel.setText("24");
+                }
+                else {
+                    ((GameActivity) getActivity()).setStrikes( (((GameActivity) getActivity()).getStrikes()+1));
+                    tvStrikes.setText("Strikes:" + ((GameActivity) getActivity()).getStrikes());
+                }
+                break;
+            case R.id.blueTriagle:
+                if(count==3 || count == 4){
+                    count++;
+                }
+                else {
+                    ((GameActivity) getActivity()).setStrikes( (((GameActivity) getActivity()).getStrikes()+1));
+                    tvStrikes.setText("Strikes:" + ((GameActivity) getActivity()).getStrikes());
+                }
+                break;
+            case R.id.redSquare:
+if(count==1 || count == 2){
+    count++;
+}
+else {
+    ((GameActivity) getActivity()).setStrikes( (((GameActivity) getActivity()).getStrikes()+1));
+    tvStrikes.setText("Strikes:" + ((GameActivity) getActivity()).getStrikes());
+}
+                break;
+            case R.id.blueCircle:
+                ((GameActivity) getActivity()).setStrikes( (((GameActivity) getActivity()).getStrikes()+1));
+                tvStrikes.setText("Strikes:" + ((GameActivity) getActivity()).getStrikes());
+                break;
+            case R.id.tvLevel:
+                if(count==7){
+                    ((GameActivity) getActivity()).setLevel(((GameActivity) getActivity()).getLevel() + 1);
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment,new FragmentLevel19()).commit();
+                }
+                else{
+                    ((GameActivity) getActivity()).setStrikes( (((GameActivity) getActivity()).getStrikes()+1));
+                    tvStrikes.setText("Strikes:" + ((GameActivity) getActivity()).getStrikes());
+                }
                 break;
         }
     }
